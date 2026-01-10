727f355d411e96ceb06654f69fe43752293e8733
Pruthviraj-333 <pruthvirajraj333@gmail.com>
2026-01-10 23:47:40 +0530

    Update main_api.py

--- backend/main_api.py
+++ backend/main_api.py
@@ -68,7 +68,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
-        "https://email-automation-system1.vercel.app/" # Add your production domain 
+        "https://email-automation-system-self.vercel.app" # Add your production domain 
     ],
     allow_credentials=True,
     allow_methods=["*"],
